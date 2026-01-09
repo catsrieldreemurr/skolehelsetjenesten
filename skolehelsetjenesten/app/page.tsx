ffae0eeccab1eb401abaff8558a8ed3613d782e5
StargazerCatsriel <danobrovold@gmail.com>
2026-01-09 13:25:24 +0100

    Finish First page's general design

--- skolehelsetjenesten/app/page.tsx
+++ skolehelsetjenesten/app/page.tsx
@@ -1,37 +1,38 @@
+import { Button } from "@/components/ui/button";
 import Navbar from "@/components/ui/navbar";
 import Typography from "@/components/ui/typography";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
 
-      <div className="flex flex-col items-center justify-center h-screen"> {/* Hero */}
-        <Typography variant="h1" isBold>Skolehelsetjenesten</Typography>
-        <Typography variant="h3">Hamar Katedralskole</Typography>
+      <div className="flex flex-col items-center justify-center h-180 backdrop-blur-none bg-[url(/cutKatta.png)] bg-cover border-b-3 border-kattaLightGreen"> {/* Hero */}
+        <span className="bg-black/50 backdrop-blur-xs p-10 rounded-md tex text-white">
+          <Typography variant="h1" isBold>Skolehelsetjenesten</Typography>
+          <Typography variant="h3">Hamar Katedralskole</Typography>
+        </span>
       </div>
 
       <div className="bg-kattaGray p-5">
-        
-        <Typography variant="h2" isBold isCentered isUnderline>Helsesykepleiere</Typography> {/* Will probably fully rework this, not happy with it */}
-        <div className="sm:flex justify-center gap-20 mt-10">
-          <div className="flex flex-col items-center justify-center bg-kattaGreen text-white p-5 rounded-sm">
+        <Typography variant="h2" isBold isCentered includeSpacer>Våre Helsesykepleiere</Typography> {/* Will probably fully rework this, not happy with it */}
+        <div className="flex flex-col justify-center gap-5 m-15 items-center">
+          <div className="flex flex-col items-center justify-center bg-kattaGreen text-white p-5 rounded-sm w-4/5 sm:w-3/5">
             <Typography isBold variant="h3">Marianne Buvik</Typography>
             <Typography>902 69 665</Typography>
-            <Image src={"/placeholder_photo.jpg"} alt="placeholder" height={500} width={300} className="rounded-xl m-5"></Image>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-kattaGreen text-white p-5 rounded-sm mt-10 sm:mt-0">
+          <div className="flex flex-col items-center justify-center bg-kattaGreen text-white p-5 rounded-sm w-4/5 sm:w-3/5">
             <Typography isBold variant="h3">Hanne Krøtøy</Typography>
             <Typography>912 48 594</Typography>
-            <Image src={"/placeholder_photo.jpg"} alt="placeholder" height={500} width={300} className="rounded-xl m-5"></Image>
           </div> 
         </div>
 
         <div className="flex justify-center items-center mt-10">
-          <div className="bg-white w-1/3 p-5 rounded-sm">
-            <Typography variant="h3" isBold isCentered>Åpningstider</Typography>
+          <div className="bg-white w-4/5 sm:w-3/5 p-5 rounded-sm text-center">
+            <Typography variant="h3" isBold isCentered>Kontortider</Typography>
             <Typography>Mandag kl. 09–14</Typography>
             <Typography>Tirsdag kl. 09–14</Typography>
             <Typography>Onsdag kl. 09–13</Typography>
@@ -40,6 +41,30 @@ export default function Home() {
           </div>
         </div>
         
+        <div className="flex flex-col sm:flex-row p-5 gap-5 justify-center mt-15">
+          <div className="bg-kattaGray2 sm:w-1/5 h-[15rem] p-5">
+            <Typography variant="h3" isBold isCentered>Hvor er vi?</Typography>
+            <Typography isCentered>Vi ligger i 3. Etasje i fløy 1 ved Elevtjenesten.</Typography>
+          </div>
+
+          <div className="bg-kattaGray2 sm:w-1/5 h-[15rem] p-5">
+            <Typography variant="h3" isBold isCentered>Bestill en Time</Typography>
+            <Typography isCentered>Ønsker du å bestille time, sender du en SMS med navn og fødselsdato til en av Helsesykepleierene, eller trykk nedenfor.</Typography>
+
+            <div className="flex items-center justify-center">
+              <Link href={"/"} className="bg-kattaLightGreen text-white p-3 rounded-xl mt-8 w-1/2 text-center">Bestill Her</Link>
+            </div>
+            
+          </div>
+
+          <div className="bg-kattaGray2 sm:w-1/5 h-[15rem] p-5">
+            <Typography variant="h3" isBold isCentered>Finn oss på Snapchat!</Typography>
+            <div className="flex justify-center items-center mt-5">
+              <Image src={"/kattaSnap.png"} height={150} width={150} alt="Snapchat Image"></Image>
+            </div>
+            
+          </div>
+        </div>
       </div>
     </div>
   );
--- skolehelsetjenesten/components/ui/typography.tsx
+++ skolehelsetjenesten/components/ui/typography.tsx
@@ -6,27 +6,58 @@ interface setTypes{
     isBold?: boolean
     isCentered?: boolean
     isUnderline?: boolean
+    includeSpacer?: boolean
     children?: ReactNode
 }
 
-export default function Typography({variant, isBold, isCentered, children, isUnderline}:setTypes){
+export default function Typography({variant, isBold, isCentered, children, isUnderline, includeSpacer}:setTypes){
     if (variant === "h1"){
-        return <h1 className={`text-4xl ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</h1>
+        return <h1 className={`text-4xl 
+                ${isBold && "font-bold"} 
+                ${isCentered && "text-center"} 
+                ${isUnderline && "underline"}
+                ${includeSpacer && "m-5"}
+                `}
+            >{children}</h1>
     }
 
     else if(variant === "h2"){
-        return <h2 className={`text-3xl ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</h2>
+        return <h2 className={`text-3xl 
+                ${isBold && "font-bold"} 
+                ${isCentered && "text-center"} 
+                ${isUnderline && "underline"}
+                ${includeSpacer && "m-5"}
+                `}
+            >{children}</h2>
     }
 
     else if(variant === "h3"){
-        return <h2 className={`text-2xl ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</h2>
+        return <h2 className={`text-2xl 
+                ${isBold && "font-bold"} 
+                ${isCentered && "text-center"} 
+                ${isUnderline && "underline"}
+                ${includeSpacer && "m-5"}
+                `}
+            >{children}</h2>
     }
 
     else if(variant === "h4"){
-        return <h2 className={`text-xl ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</h2>
+        return <h2 className={`text-xl 
+                ${isBold && "font-bold"} 
+                ${isCentered && "text-center"} 
+                ${isUnderline && "underline"}
+                ${includeSpacer && "m-5"}
+                `}
+            >{children}</h2>
     }
 
     else {
-        return <p className={`text-lg ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</p>
+        return <p className={`text-lg 
+                ${isBold && "font-bold"} 
+                ${isCentered && "text-center"} 
+                ${isUnderline && "underline"}
+                ${includeSpacer && "m-5"}
+                `}
+            >{children}</p>
     }
 }
