1a67bedea826fc9e8e6654eb1525badcdfffb827
StargazerCatsriel <danobrovold@gmail.com>
2026-01-09 09:45:49 +0100

    Add Test Layout

--- skolehelsetjenesten/app/page.tsx
+++ skolehelsetjenesten/app/page.tsx
@@ -7,7 +7,40 @@ export default function Home() {
     <div>
       <Navbar></Navbar>
 
-      <Typography isCentered>tes</Typography>
+      <div className="flex flex-col items-center justify-center h-screen"> {/* Hero */}
+        <Typography variant="h1" isBold>Skolehelsetjenesten</Typography>
+        <Typography variant="h3">Hamar Katedralskole</Typography>
+      </div>
+
+      <div className="bg-kattaGray p-5">
+        
+        <Typography variant="h2" isBold isCentered isUnderline>Helsesykepleiere</Typography> {/* Will probably fully rework this, not happy with it */}
+        <div className="sm:flex justify-center gap-20 mt-10">
+          <div className="flex flex-col items-center justify-center bg-kattaGreen text-white p-5 rounded-sm">
+            <Typography isBold variant="h3">Marianne Buvik</Typography>
+            <Typography>902 69 665</Typography>
+            <Image src={"/placeholder_photo.jpg"} alt="placeholder" height={500} width={300} className="rounded-xl m-5"></Image>
+          </div>
+
+          <div className="flex flex-col items-center justify-center bg-kattaGreen text-white p-5 rounded-sm mt-10 sm:mt-0">
+            <Typography isBold variant="h3">Hanne Krøtøy</Typography>
+            <Typography>912 48 594</Typography>
+            <Image src={"/placeholder_photo.jpg"} alt="placeholder" height={500} width={300} className="rounded-xl m-5"></Image>
+          </div> 
+        </div>
+
+        <div className="flex justify-center items-center mt-10">
+          <div className="bg-white w-1/3 p-5 rounded-sm">
+            <Typography variant="h3" isBold isCentered>Åpningstider</Typography>
+            <Typography>Mandag kl. 09–14</Typography>
+            <Typography>Tirsdag kl. 09–14</Typography>
+            <Typography>Onsdag kl. 09–13</Typography>
+            <Typography>Torsdag kl. 09–14</Typography>
+            <Typography>Fredag kl. 09–11</Typography>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
--- skolehelsetjenesten/components/ui/typography.tsx
+++ skolehelsetjenesten/components/ui/typography.tsx
@@ -5,27 +5,28 @@ interface setTypes{
     variant?: string
     isBold?: boolean
     isCentered?: boolean
+    isUnderline?: boolean
     children?: ReactNode
 }
 
-export default function Typography({variant, isBold, isCentered, children}:setTypes){
+export default function Typography({variant, isBold, isCentered, children, isUnderline}:setTypes){
     if (variant === "h1"){
-        return <h1 className={`text-4xl ${isBold && "font-bold"} ${isCentered && "text-center"}`}>{children}</h1>
+        return <h1 className={`text-4xl ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</h1>
     }
 
     else if(variant === "h2"){
-        return <h2 className={`text-3xl ${isBold && "font-bold"} ${isCentered && "text-center"}`}>{children}</h2>
+        return <h2 className={`text-3xl ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</h2>
     }
 
     else if(variant === "h3"){
-        return <h2 className={`text-2xl ${isBold && "font-bold"} ${isCentered && "text-center"}`}>{children}</h2>
+        return <h2 className={`text-2xl ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</h2>
     }
 
     else if(variant === "h4"){
-        return <h2 className={`text-xl ${isBold && "font-bold"} ${isCentered && "text-center"}`}>{children}</h2>
+        return <h2 className={`text-xl ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</h2>
     }
 
     else {
-        return <p className={`text-lg ${isBold && "font-bold"} ${isCentered && "text-center"}`}>{children}</p>
+        return <p className={`text-lg ${isBold && "font-bold"} ${isCentered && "text-center"} ${isUnderline && "underline"}`}>{children}</p>
     }
 }
